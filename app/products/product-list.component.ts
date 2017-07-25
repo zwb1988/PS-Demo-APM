47fa35648655c8fd66c0fed2ba2796b12a75c9da
zwb1988 <zwb1988@hotmail.com>
2017-07-25 23:18:57 +0800

    Nested component

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -32,7 +32,7 @@ export class ProductListComponent implements OnInit {
             "releaseDate": "March 18, 2016",
             "description": "15 gallon capacity rolling garden cart",
             "price": 32.99,
-            "starRating": 4.2,
+            "starRating": 4.8,
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
         }
     ];
@@ -43,5 +43,9 @@ export class ProductListComponent implements OnInit {
     
     ngOnInit(): void {
         console.log('In OnInit');
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
     }
 }
