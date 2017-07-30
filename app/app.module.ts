6bca983f32f0c3c403ec4398b8194df97a90bed0
zwb1988 <zwb1988@hotmail.com>
2017-07-30 22:46:19 +0800

    DI of service and HTTP service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
@@ -9,7 +11,8 @@ import { StarComponent } from './shared/star.component';
 @NgModule({
   imports: [ 
     BrowserModule, 
-    FormsModule 
+    FormsModule,
+    HttpModule
   ],
   declarations: [ 
     AppComponent, 
