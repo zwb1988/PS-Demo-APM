591f0c2afdc19488f07ac3ed0e269a2a24683ce5
zwb1988 <zwb1988@hotmail.com>
2017-08-06 23:36:17 +0800

    Routing basics

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,9 +5,17 @@ import { ProductService } from './products/product.service';
     selector: 'pm-app',
     template: `
     <div>
-        <h1>{{pageTitle}}</h1>
-        <pm-products></pm-products>
+        <nav class='navbar navbar-default'>
+            <div class='container-fluid'>
+                <a class='navbar-brand'>{{ pageTitle }}</a>
+                <ul class='nav navbar-nav'>
+                    <li><a [routerLink]="['/welcome']">Home</a></li>
+                    <li><a [routerLink]="['/products']">Product List</a></li>
+                </ul>
+            </div>
+        </nav>
     </div>
+    <router-outlet></router-outlet>
     `,
     providers: [ProductService]
 })
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,24 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
 
 @NgModule({
-  imports: [ 
-    BrowserModule, 
+  imports: [
+    BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ], { useHash: false })
   ],
-  declarations: [ 
-    AppComponent, 
-    ProductListComponent, 
-    ProductFilterPipe, 
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    ProductListComponent,
+    ProductFilterPipe,
+    ProductDetailComponent,
     StarComponent
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
